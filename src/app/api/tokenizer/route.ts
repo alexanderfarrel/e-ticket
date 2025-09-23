cd196e64e1b9006688da2d2abed27d2d07790526
Alex Macbook <alexbusiness.code@gmail.com>
2025-09-23 14:16:40 +0700

    setting next_public_

--- src/app/api/tokenizer/route.ts
+++ src/app/api/tokenizer/route.ts
@@ -5,8 +5,8 @@ import validator from "validator";
 
 const snap = new Midtrans.Snap({
   isProduction: false,
-  serverKey: process.env.NEXT_PUBLIC_MIDTRANS_SERVER_KEY ?? "",
-  clientKey: process.env.NEXT_PUBLIC_MIDTRANS_CLIENT_KEY ?? "",
+  serverKey: process.env.MIDTRANS_SERVER_KEY ?? "",
+  clientKey: process.env.MIDTRANS_CLIENT_KEY ?? "",
 });
 
 export async function POST(req: NextRequest) {
