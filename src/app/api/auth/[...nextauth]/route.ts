0b575a469a101c9762b9f81b68099b280fd81b7a
Alex Macbook <alexbusiness.code@gmail.com>
2025-09-23 13:49:04 +0700

    i think the error is about nextauth_secret cause different

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,4 @@
-import { authOptions } from "../../../../libs/auth/auth";
+import { authOptions } from "@/libs/auth/auth";
 import NextAuth from "next-auth";
 
 const handler = NextAuth(authOptions);
