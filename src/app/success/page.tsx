6394cb8fe4172467bb2a7d0960c06e8b51ae9661
Alex Macbook <alexbusiness.code@gmail.com>
2025-10-01 09:54:59 +0700

    customize success page

--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -1,27 +1,86 @@
+"use client";
 import { Link } from "next-view-transitions";
+import { useSearchParams } from "next/navigation";
 
-export default function successPage() {
+export default function SuccessPage() {
+  const searchParams = useSearchParams();
+  const transactionStatus = searchParams.get("transaction_status");
+  const statusCode = searchParams.get("status_code");
   return (
-    <main className="grid min-h-screen place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
-      <div className="text-center">
-        <p className="font-semibold text-green-500 text-[4rem] leading-[3rem] max-sm:text-3xl">
-          Pembayaran Berhasil
-        </p>
-        <h1 className="mt-4 text-7xl font-semibold tracking-tight text-balance text-gray-900 max-sm:text-2xl max-sm:mt-0">
-          Terimakasih Atas Pembeliannya
-        </h1>
-        <p className="mt-6 text-lg font-medium text-pretty text-orange-500 sm:text-xl/8 max-sm:text-base max-sm:mt-1">
-          {`Cek folder spam jika email tidak muncul di inbox`}
-        </p>
-        <div className="mt-10 flex items-center justify-center gap-x-6 max-sm:mt-4">
-          <Link
-            href="/"
-            className="border-2 rounded-xl bg-green-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-green-500/80 max-sm:py-2"
-          >
-            Kembali ke Beranda
-          </Link>
-        </div>
-      </div>
-    </main>
+    <>
+      {transactionStatus === "settlement" && statusCode === "200" ? (
+        <main className="grid min-h-screen place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+          <div className="text-center">
+            <p className="font-semibold text-green-500 text-[4rem] leading-[3rem] max-sm:text-3xl">
+              Pembayaran Berhasil
+            </p>
+            <h1 className="mt-4 text-7xl font-semibold tracking-tight text-balance text-gray-900 max-sm:text-2xl max-sm:mt-0">
+              Terimakasih Atas Pembeliannya
+            </h1>
+            <p className="mt-6 text-lg font-medium text-pretty text-orange-500 sm:text-xl/8 max-sm:text-base max-sm:mt-1">
+              {`Cek folder spam jika email tidak muncul di inbox`}
+            </p>
+            <div className="mt-10 flex items-center justify-center gap-x-6 max-sm:mt-4">
+              <Link
+                href="/"
+                className="border-2 rounded-xl bg-green-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-green-500/80 max-sm:py-2"
+              >
+                Kembali ke Beranda
+              </Link>
+            </div>
+          </div>
+        </main>
+      ) : transactionStatus === "pending" && statusCode === "201" ? (
+        <main className="grid min-h-screen place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+          <div className="text-center">
+            <p className="font-semibold text-orange-400 text-[4rem] leading-[3rem] max-sm:text-3xl">
+              Payment Pending
+            </p>
+            <h1 className="mt-4 text-7xl font-semibold tracking-tight text-balance text-gray-900 max-sm:text-2xl max-sm:mt-0">
+              Pembayaranmu Masih Tertunda
+            </h1>
+            <p className="mt-6 text-lg font-medium text-pretty text-orange-500 sm:text-xl/8 max-sm:text-base max-sm:mt-1">
+              {`Kamu bisa melanjutkan pembayaran atau membuat pembayaran baru`}
+            </p>
+            <div className="mt-10 flex items-center justify-center gap-x-6 max-sm:mt-4">
+              <Link
+                href="/"
+                className="border-2 rounded-xl bg-orange-400 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-orange-500 max-sm:py-2"
+              >
+                Kembali ke Beranda
+              </Link>
+            </div>
+          </div>
+        </main>
+      ) : (
+        <main className="grid min-h-screen place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+          <div className="text-center">
+            <p className="font-semibold text-red-500 text-[4rem] leading-[3rem] max-sm:text-3xl">
+              Terjadi Kesalahan
+            </p>
+            <h1 className="mt-4 text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl max-sm:text-2xl max-sm:mt-0">
+              Mohon Maaf Atas Kendala yang terjadi
+            </h1>
+            <p className="mt-6 text-lg font-medium text-pretty text-orange-500 sm:text-xl/8 max-sm:text-base max-sm:mt-1">
+              {`Silahkan hubungi panitia kami dibawah ini untuk memproses kendalamu lebih lanjut`}
+            </p>
+            <div className="mt-10 flex items-center justify-center gap-x-6 max-sm:mt-4">
+              <Link
+                href="/"
+                className="border-2 rounded-xl bg-blue-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-blue-500/80 max-sm:py-2"
+              >
+                Kembali ke Beranda
+              </Link>
+              <Link
+                href="https://wa.me/+6289680575400"
+                className="border-2 rounded-xl bg-green-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-green-500/80 max-sm:py-2"
+              >
+                Hubungi Panitia
+              </Link>
+            </div>
+          </div>
+        </main>
+      )}
+    </>
   );
 }
