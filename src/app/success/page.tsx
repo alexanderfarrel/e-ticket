a005e65ae1a4ceb2fffba0b2cac9aee372930b2e
Alex Macbook <alexbusiness.code@gmail.com>
2025-10-01 16:01:34 +0700

    feat[success]: add button to call admin if the user has any problem with the transaction

--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -18,7 +18,10 @@ export default function SuccessPage() {
               Terimakasih Atas Pembeliannya
             </h1>
             <p className="mt-6 text-lg font-medium text-pretty text-orange-500 sm:text-xl/8 max-sm:text-base max-sm:mt-1">
-              {`Cek folder spam jika email tidak muncul di inbox`}
+              {`Cek folder spam jika email tidak muncul di inbox!`}
+            </p>
+            <p className="text-lg font-medium text-pretty text-black-500 sm:text-xl/8 max-sm:text-base max-sm:mt-1">
+              {`Hubungi panitia kami dibawah ini jika mengalami kendala`}
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6 max-sm:mt-4">
               <Link
@@ -26,6 +29,12 @@ export default function SuccessPage() {
                 className="border-2 rounded-xl bg-green-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-green-500/80 max-sm:py-2"
               >
                 Kembali ke Beranda
+              </Link>
+              <Link
+                href="https://wa.me/+6289680575400"
+                className="border-2 rounded-xl bg-orange-400 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-orange-400/80 max-sm:py-2"
+              >
+                Hubungi Panitia
               </Link>
             </div>
           </div>
